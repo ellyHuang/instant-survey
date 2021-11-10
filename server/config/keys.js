1eac189f9842517e6519579ea932a567c8065226
Elly <cu0710cu0710@gmail.com>
2021-11-10 14:44:37 +0800

    add authentication and db setting

---
+++ server/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '154388897043-aua50cgacckqgfth6t3hj38cmap79da2.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://dbUser001:tYrJ9FHPQSRsM2Bt@instant-survey.avhy5.mongodb.net/Instant-Survey?retryWrites=true&w=majority',
+    cookieKey: '1puhwqkjnerhrgljkebrljhqleibljlkjnkvjebeq'
+}
